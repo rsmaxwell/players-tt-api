eeee87fedef199c4a20f69e7701eca8f67baf3f3
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-11-20 09:38:48 +0000

    test on alpine

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,11 @@ go build -o ${BUILD_DIR}/ ./...
 cat /etc/os-release
 cp /etc/os-release ${BUILD_DIR}/os-release
 
+cat << EOF > ${BUILD_DIR}/info.json
+{
+   "version":${BUILD_ID}
+}
+EOF    
+
 ls -al ${BUILD_DIR}
 
