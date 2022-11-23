475714b05acea2c8904f5d209386a3ae928b5af8
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-11-23 21:16:40 +0000

    test on alpine

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,11 @@
 set -x 
 
 BUILD_DIR=./build
+INFO_DIR=./${BUILD_DIR}/info
 
 go build -o ${BUILD_DIR}/ ./...
 
-cat /etc/os-release
-cp /etc/os-release ${BUILD_DIR}/os-release
+cp /etc/os-release ${INFO_DIR}/os-release
 
 ls -al ${BUILD_DIR}
-
+ls -al ${INFO_DIR}
