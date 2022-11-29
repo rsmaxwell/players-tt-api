7957e97dd3cf0cc05ac4c5db54a8c93089062564
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-11-29 14:02:59 +0000

    Create test data people as inactive

--- cmd/players-tt-api-testdata/main.go
+++ cmd/players-tt-api-testdata/main.go
@@ -96,26 +96,26 @@ func makePeopleTx(db *sql.DB) (map[int]int, error) {
 	f := functionMakePeopleTx
 
 	peopleData := []PersonData{
-		{Data: model.Registration{FirstName: "James", LastName: "Bond", Knownas: "007", Email: "007@mi6.gov.uk", Phone: "01632 960573", Password: "TopSecret123"}, Status: model.StatusPlayer},
-		{Data: model.Registration{FirstName: "Alice", LastName: "Frombe", Knownas: "ali", Email: "ali@mikymouse.com", Phone: "01632 960372", Password: "ali1234567"}, Status: model.StatusPlayer},
-		{Data: model.Registration{FirstName: "Tom", LastName: "Smith", Knownas: "tom", Email: "tom@hotmail.com", Phone: "01632 960512", Password: "tom12378909876"}, Status: model.StatusPlayer},
+		{Data: model.Registration{FirstName: "James", LastName: "Bond", Knownas: "007", Email: "007@mi6.gov.uk", Phone: "01632 960573", Password: "TopSecret123"}, Status: model.StatusInactive},
+		{Data: model.Registration{FirstName: "Alice", LastName: "Frombe", Knownas: "ali", Email: "ali@mikymouse.com", Phone: "01632 960372", Password: "ali1234567"}, Status: model.StatusInactive},
+		{Data: model.Registration{FirstName: "Tom", LastName: "Smith", Knownas: "tom", Email: "tom@hotmail.com", Phone: "01632 960512", Password: "tom12378909876"}, Status: model.StatusInactive},
 		{Data: model.Registration{FirstName: "Sandra", LastName: "Smythe", Knownas: "tom", Email: "sandra@hotmail.com", Phone: "01632 960966", Password: "sandra12334567"}, Status: model.StatusInactive},
-		{Data: model.Registration{FirstName: "George", LastName: "Washington", Knownas: "george", Email: "george@hotmail.com", Phone: "01632 960278", Password: "george789"}, Status: model.StatusPlayer},
-		{Data: model.Registration{FirstName: "Margret", LastName: "Tiffington", Knownas: "maggie", Email: "marg@hotmail.com", Phone: "01632 960165", Password: "magie876"}, Status: model.StatusPlayer},
+		{Data: model.Registration{FirstName: "George", LastName: "Washington", Knownas: "george", Email: "george@hotmail.com", Phone: "01632 960278", Password: "george789"}, Status: model.StatusInactive},
+		{Data: model.Registration{FirstName: "Margret", LastName: "Tiffington", Knownas: "maggie", Email: "marg@hotmail.com", Phone: "01632 960165", Password: "magie876"}, Status: model.StatusInactive},
 		{Data: model.Registration{FirstName: "James", LastName: "Ernest", Knownas: "jamie", Email: "jamie@ntlworld.com", Phone: "01632 960757", Password: "jamie5293645284"}, Status: model.StatusInactive},
-		{Data: model.Registration{FirstName: "Elizabeth", LastName: "Tudor", Knownas: "liz", Email: "liz@buck.palice.com", Phone: "01632 960252", Password: "liz1756453423"}, Status: model.StatusPlayer},
+		{Data: model.Registration{FirstName: "Elizabeth", LastName: "Tudor", Knownas: "liz", Email: "liz@buck.palice.com", Phone: "01632 960252", Password: "liz1756453423"}, Status: model.StatusInactive},
 		{Data: model.Registration{FirstName: "Dick", LastName: "Whittington", Knownas: "dick", Email: "dick@ntlworld.com", Phone: "01746 352413", Password: "dick3296846734524"}, Status: model.StatusPlayer},
-		{Data: model.Registration{FirstName: "Victoria", LastName: "Hempworth", Knownas: "vickie", Email: "vickie@waitrose.com", Phone: "0195 76863241", Password: "vickie846"}, Status: model.StatusPlayer},
-		{Data: model.Registration{FirstName: "Shanika", LastName: "Pierre", Knownas: "pete", Email: "IcyGamer@gmail.com", Phone: "01632 960576", Password: "Top12345Secret"}, Status: model.StatusPlayer},
+		{Data: model.Registration{FirstName: "Victoria", LastName: "Hempworth", Knownas: "vickie", Email: "vickie@waitrose.com", Phone: "0195 76863241", Password: "vickie846"}, Status: model.StatusInactive},
+		{Data: model.Registration{FirstName: "Shanika", LastName: "Pierre", Knownas: "pete", Email: "IcyGamer@gmail.com", Phone: "01632 960576", Password: "Top12345Secret"}, Status: model.StatusInactive},
 		{Data: model.Registration{FirstName: "Wanangwa", LastName: "Czajkowski", Knownas: "wan", Email: "torphy.dayana@dicki.com", Phone: "01632 960628", Password: "ali12387654"}, Status: model.StatusPlayer},
 		{Data: model.Registration{FirstName: "Cormac", LastName: "Dwight", Knownas: "cor", Email: "adela.kunze@schmitt.com", Phone: "01632 960026", Password: "tom123frgthyj"}, Status: model.StatusSuspended},
 		{Data: model.Registration{FirstName: "Ramóna", LastName: "Jonker", Knownas: "ram", Email: "ariel07@hotmail.com", Phone: "01632 960801", Password: "sandra123frr"}, Status: model.StatusSuspended},
 		{Data: model.Registration{FirstName: "Quinctilius", LastName: "Jack", Knownas: "qui", Email: "kara.johnston@runte.com", Phone: "01632 960334", Password: "george789ed5"}, Status: model.StatusInactive},
 		{Data: model.Registration{FirstName: "Radu", LastName: "Godfrey", Knownas: "rad", Email: "ella.vonrueden@kuhic.com", Phone: "01632 960450", Password: "magie87689ilom"}, Status: model.StatusSuspended},
-		{Data: model.Registration{FirstName: "Aleksandrina", LastName: "Couture", Knownas: "ale", Email: "archibald.stark@hotmail.com", Phone: "01632 960928", Password: "jamie529re5gb"}, Status: model.StatusPlayer},
+		{Data: model.Registration{FirstName: "Aleksandrina", LastName: "Couture", Knownas: "ale", Email: "archibald.stark@hotmail.com", Phone: "01632 960928", Password: "jamie529re5gb"}, Status: model.StatusInactive},
 		{Data: model.Registration{FirstName: "Catrin", LastName: "Wooldridge", Knownas: "cat", Email: "sauer.luciano@hotmail.com", Phone: "01632 960126", Password: "liz14rdgujmbvr43"}, Status: model.StatusSuspended},
-		{Data: model.Registration{FirstName: "Souleymane", LastName: "Walter", Knownas: "sou", Email: "damon.toy@swaniawski.com", Phone: "01632 960403", Password: "dick3287uyh5fredw"}, Status: model.StatusPlayer},
-		{Data: model.Registration{FirstName: "Dorotėja", LastName: "Antúnez", Knownas: "dor", Email: "omante@marks.com", Phone: "01632 961252", Password: "vickie846y6"}, Status: model.StatusPlayer},
+		{Data: model.Registration{FirstName: "Souleymane", LastName: "Walter", Knownas: "sou", Email: "damon.toy@swaniawski.com", Phone: "01632 960403", Password: "dick3287uyh5fredw"}, Status: model.StatusInactive},
+		{Data: model.Registration{FirstName: "Dorotėja", LastName: "Antúnez", Knownas: "dor", Email: "omante@marks.com", Phone: "01632 961252", Password: "vickie846y6"}, Status: model.StatusInactive},
 	}
 
 	peopleIDs := make(map[int]int)
